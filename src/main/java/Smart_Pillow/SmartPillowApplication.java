cba91d8496bf4c0a6b1f2550d4952b19c21a65f1
Gabriel <jotagabi01@gmail.com>
2025-11-24 17:03:50 +0100

    Proyecto

---
+++ src/main/java/Smart_Pillow/SmartPillowApplication.java
@@ -0,0 +1,13 @@
+package Smart_Pillow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPillowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPillowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Smart_Pillow/SmartPillowApplicationTests.java
@@ -0,0 +1,13 @@
+package Smart_Pillow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPillowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
