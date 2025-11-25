fce1b86ec19de5410abf96308bf22d036f396db6
Gabriel <0610697543@uma.es>
2025-11-25 17:05:13 +0100

    feat: Implementado Controlador1

---
+++ src/main/java/Smart_Pillow/Controlador1.java
@@ -0,0 +1,14 @@
+package Smart_Pillow;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controlador1 {
+
+    @GetMapping("/hola")
+    public String decirHola() {
+        return "¡Hola Mundo! Este es mi primer controlador en Spring Boot.";
+    }
+}
