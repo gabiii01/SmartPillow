28b3b3adb1e1909ae3f2f314dfd22c56d50679e5
Natalia Rivas <nataliarivasmorales01@gmail.com>
2025-12-03 19:41:36 +0100

    Añadido controlador HolaController

--- src/main/java/Smart_Pillow/HolaController.java
+++ src/main/java/Smart_Pillow/HolaController.java
@@ -4,11 +4,12 @@ package Smart_Pillow;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class Controlador1 {
 
-    @GetMapping("/hola")
-    public String decirHola() {
-        return "¡Hola Mundo! Este es mi primer controlador en Spring Boot.";
+@RestController
+public class HolaController {
+
+    @GetMapping("/natalia")
+    public String hola() {
+        return "¡Hola desde la rama de Natalia!";
     }
 }
