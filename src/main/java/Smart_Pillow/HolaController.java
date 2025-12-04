02b7f5660b118f4c058e8f6ad4477047ad2c752f
Gabriel <0610697543@uma.es>
2025-12-04 18:52:18 +0100

    Error resuelto

--- src/main/java/Smart_Pillow/HolaController.java
+++ src/main/java/Smart_Pillow/HolaController.java
@@ -5,17 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-<<<<<<< HEAD:src/main/java/Smart_Pillow/Controlador1.java
-    @GetMapping("/hola")
-    public String decirHola() {
-        return "¡Hola Mundo! Este es mi primer controlador, en Spring Boot.";
-=======
 @RestController
 public class HolaController {
 
     @GetMapping("/natalia")
     public String hola() {
         return "Hola desde la rama de Natalia";
->>>>>>> a2b4aacc1cfaa9865e206a17e5654e413db7e967:src/main/java/Smart_Pillow/HolaController.java
+
     }
 }
